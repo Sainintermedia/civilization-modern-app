e2bce92028e7ac7d69f4ac66591698e74f3b8a31
ahmad holidin <akholidin0@gmail.com>
2022-11-16 21:56:55 +0700

    ahmad

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,85 @@
 @extends('layouts.main')
-
+@section('title', 'Dashboard')
+@section('breadcrumb', 'Dashboard')
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="wrapper">
+    <div class="row">
+        <div class="col-12">
+            <div class="card card-primary card-outline">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-3 col-6">
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
+                            <div class="small-box bg-warning">
+                                <div class="inner">
+                                    <h3>44</h3>
+                                    <p>Wilayah Kejaroan</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="icon ion-location"></i>
+                                </div>
+                                <a href="#" class="small-box-footer">More info <i
+                                        class="fas fa-arrow-circle-right"></i></a>
                             </div>
-                        @endif
+                        </div>
 
-                        {{ __('You are logged in!') }}
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-purple">
+                                <div class="inner">
+                                    <h3>44</h3>
+                                    <p>Kependudukan</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-person-add"></i>
+                                </div>
+                                <a href="#" class="small-box-footer">More info <i
+                                        class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-primary">
+                                <div class="inner">
+                                    <h3>44</h3>
+                                    <p>Keluarga</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-ios-people"></i>
+                                </div>
+                                <a href="#" class="small-box-footer">More info <i
+                                        class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-info">
+                                <div class="inner">
+                                    <h3>44</h3>
+                                    <p>Surat Tercetak</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion-ios-paper"></i>
+                                </div>
+                                <a href="#" class="small-box-footer">More info <i
+                                        class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+
                     </div>
+
                 </div>
             </div>
         </div>
     </div>
+</div>
+
+@endsection
+
+@endsection
+@section('styles')
+
+@endsection
+
+@section('javas')
+
 @endsection
