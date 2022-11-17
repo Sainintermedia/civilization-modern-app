330bf1fa63a2f7ee595418246a50ac8b05b72713
ahmad holidin <akholidin0@gmail.com>
2022-11-17 19:00:46 +0700

    ahmad

--- resources/views/frontend/main.blade.php
+++ resources/views/frontend/main.blade.php
@@ -28,8 +28,8 @@
     
     <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
     <div class="container">
-    <a href="../../index3.html" class="navbar-brand">
-    <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <a href="/front" class="navbar-brand">
+    <img  src="{!! URL::asset('assets/admin/dist/img/logo.png') !!}"  alt="Desa Cisoka" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
     <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
