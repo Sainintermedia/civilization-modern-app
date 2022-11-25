d015672d5d766a405446b450d3e9874fa12c1e37
hiarief <arhistr14@gmail.com>
2022-11-25 20:13:21 +0700

    arief

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -20,7 +20,7 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1 class="m-0">@yield('title')</h1>
+                            <h4 class="m-0">@yield('title')</h4>
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
