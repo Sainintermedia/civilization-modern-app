5691904ffb42334142889ff6006a39d3ebae15e4
hiarief <arief.hidayat.97@gmail.com>
2022-12-04 19:41:24 +0700

    arief

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,12 +18,6 @@ class DashboardController extends Controller
         $jenispekerjaan = Work::orderBy('nama')->get();
         $rt1 = FamillyCard::get()->groupBy('rt');
         $rw = FamillyCard::get()->groupBy('rw');
-        // return $rt1;
-        // $rt2 = FamillyCard::whereRt(2)->count();
-        // $rt3 = FamillyCard::whereRt(3)->count();
-        // $rt4 = FamillyCard::whereRt(4)->count();
-        // $rt5 = FamillyCard::whereRt(5)->count();
-        // $rt6 = FamillyCard::whereRt(6)->count();
         return view('dashboard.index', compact('famillycards', 'famillycardmembers','jenispekerjaan', 'rt1', 'rw'));
     }
 }
